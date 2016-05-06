0a0a32919f7e5d43074e6f7ceef55bb790613cbb
trittimo <michaeltrittin@gmail.com>
2016-05-06 07:59:01 -0400

    integrated recipes with database

--- FlaskPackage/FOOODServer.py
+++ FlaskPackage/FOOODServer.py
@@ -5,6 +5,35 @@ import pypyodbc as db
 
 app = Flask(__name__)
 
+def getIngredients(recipeID, connection):
+    cursor = connection.cursor()
+    cursor.execute('SELECT Amount, IngredientName '
+                    'FROM Uses '
+                    'WHERE RecipeID=' + recipeID)
+    results = cursor.fetchall()
+    ingredients = []
+    for row in results:
+        ingredients.append(row[0] + ' ' + row[1])
+    return ingredients
+
+def getInstructions(recipeID, connection):
+    cursor = connection.cursor()
+    cursor.execute('SELECT Instructions '
+                   'FROM Recipe '
+                   'WHERE RecipeID=' + recipeID)
+    results = cursor.fetchall()
+    instructions = results[0][0].strip().split('\n')
+    # del instructions[1:]
+    return instructions
+
+def getTitle(recipeID, connection):
+    cursor = connection.cursor()
+    cursor.execute('SELECT RecipeName '
+                   'FROM Recipe '
+                   'WHERE RecipeID=' + recipeID)
+    results = cursor.fetchall()
+    return results[0][0].strip()
+
 @app.route('/recipe.html', methods = ['GET'])
 def recipe():
     try:
@@ -21,18 +50,17 @@ def recipe():
                             'Database=The_FOOOD;'
                             'uid=' + username +
                             ';pwd=' + password)
-    cursor = connection.cursor()
 
     recipe_id = request.args.get('recipeid', '0')
    
-    SQLCommand = "SELECT Taste FROM Ingredient WHERE Name = '" + ingredient + "'"
-    cursor.execute(SQLCommand)
-    results = cursor.fetchall()
+    ingredients = getIngredients(recipe_id, connection)
+    instructions = getInstructions(recipe_id, connection)
+    title = getTitle(recipe_id, connection)
     connection.close()
 
-    ingredients = ["1 x Egg", "1 x Ham", "1 Liter Green Food Coloring"]
-    instructions = ["Cook the egg", "Roast the ham", "Apply green food coloring", "Serve"]
-    title = "Green Eggs and Ham"
+    # ingredients = ["1 x Egg", "1 x Ham", "1 Liter Green Food Coloring"]
+    # instructions = ["Cook the egg", "Roast the ham", "Apply green food coloring", "Serve"]
+    # title = "Green Eggs and Ham"
 
     return render_template('recipe.html',
                             ingredients = ingredients,
