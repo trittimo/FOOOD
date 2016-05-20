3ef48c050551dad5db75b768ae7a81e9268d84e3
trittimo <michaeltrittin@gmail.com>
2016-05-19 20:46:27 -0400

    Things coming along

--- FlaskPackage/recipes.py
+++ FlaskPackage/recipes.py
@@ -32,6 +32,7 @@ def show_instructions():
 
     recipe_id = flask.request.args.get('recipeid')
     if not recipe_id:
+        connection.close()
         return flask.render_template('recipes/recipe.html', title="Unknown Recipe")
 
     ingredients = get_ingredients(recipe_id, connection)
@@ -44,10 +45,29 @@ def show_instructions():
                                  instructions=instructions,
                                  title=title)
 
-@BLUEPRINT.route('/search-by-ingredients.html', methods=['POST'])
+@BLUEPRINT.route('/search-by-ingredients.html', methods=['GET','POST'])
 def search_by_ingredients():
     return flask.render_template('recipes/search-by-ingredients.html')
 
+def find_recipes_with_name(name, connection):
+    if not name:
+        return []
+    cursor = connection.cursor()
+    cursor.execute(constants.SELECT_RECIPES_BY_NAME, [name])
+    results = cursor.fetchall()
+    recipes = []
+    for row in results:
+        recipes.append({'name':row[1],'price':round(row[3],2),'id':row[0]})
+
+    return recipes
+
+
 @BLUEPRINT.route('/search-by-name.html', methods=['GET'])
 def search_by_name():
-    return flask.render_template('recipes/search-by-name.html')
+    connection = db.connect(constants.DB_DATA.format(constants.DB_USERNAME, constants.DB_PASSWORD))
+    
+    recipe = flask.request.args.get('recipe')
+    result = find_recipes_with_name(recipe, connection)
+    
+    connection.close()
+    return flask.render_template('recipes/search-by-name.html', recipes=result)
